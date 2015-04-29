7903a99c70f5de14f0d28825c3d7b3aa8c0402fb
Matthieu Bulté <matthieu.bulte.06@gmail.com>
2015-04-29 11:46:07 +0200

    Added examples to Main

--- src/example/Main.purs
+++ src/example/Main.purs
@@ -2,6 +2,7 @@ module Main where
 
 import Data.Maybe
 import Debug.Trace
+import Control.Apply
 import Control.Monad.Eff
 import Control.Monad.Aff
 import Control.Monad.Aff.AVar
@@ -19,32 +20,50 @@ body request = go ""
                  Just rest -> go (message ++ rest)
                  Nothing -> return message
 
-file :: forall e. Application e
-file request respond = respond $ ResponseFile status200 [contentType "text/html"] "./src/index.html"
-
-echoApplication :: forall e. Application e
-echoApplication request respond = do
+-- Application echo-ing the received content
+echo :: forall e. Application e
+echo request respond = do
     message <- body request
     respond $ ResponseString status200 [] message
 
 
-countApplication :: forall e. AVar Number -> Application (trace :: Trace | e)
-countApplication count request respond = do
-    c <- takeVar count
-    putVar count (c + 1)
+-- Application counting the number of requests received
+count :: forall e. Wai e (Application e)
+count = do
+    count <- makeVar
+    putVar count 0
 
-    let message = "Request n°" ++ (show c)
-
-    liftEff' $ trace message
-    respond $ ResponseString status200 [] (show c)
-
-
-main :: forall e. WaiEff (trace :: Trace | e)
-main = do
-    trace "running purescript-wai on port 3001..."
-    run 3001 file
+    return $ \request respond -> do
+        c <- takeVar count
+        putVar count (c + 1)
+        respond $ ResponseString status200 [] (show c)
 
 
+-- Application serving a static index.html file
+file :: forall e. Application e
+file request respond = case request.rawPathInfo of
+  "/"     -> respond index
+  _       -> respond notFound
 
+index = ResponseFile
+    status200
+    [contentType "text/html"]
+    "./src/example/index.html"
 
+notFound = ResponseString
+    status404
+    [contentType "text/plain"]
+    "404 - Not Found"
 
+-- Composed application, serve dynamic pages and fallback to static pages
+composed = do
+    counter <- count
+    return \request respond -> case request.rawPathInfo of
+        "/count" -> counter request respond
+        "/echo"  -> echo request respond
+        _        -> file request respond
+
+main = runAff
+        (\_           -> trace "error while launching aplpication")
+        (\application -> trace "starting on port 3001..." *> run 3001 application)
+        composed
